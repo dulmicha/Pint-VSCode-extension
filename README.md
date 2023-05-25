5e8d9d8bfceb5b1ffa79493500c6162585cb30c9
dulmicha <dulmicha@gmail.com>
2023-05-25 18:44:26 +0200

    Commands
    Transpiling (+running) pint file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Pint VSCode extension
- Extension to help programming in Pint
+Extension to help programming in Pint
+
+## User interface
+Beautiful and responsive interface accessible through sidebar helps to choose emojis.
+
+## Commands
+Note: there are commands available at `Ctrl+Shift+P`, but they require debrewer and python to be accessible through command line (added to environment variables).
--- src/extension.ts
+++ src/extension.ts
@@ -23,6 +23,51 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pint.debrew', () => {
+			var terminal = vscode.window.activeTerminal;
+			var fileName = vscode.window.activeTextEditor?.document.fileName;
+			if (terminal) {
+				terminal.show();
+				terminal.sendText(`debrewer ${fileName}`);
+			} 
+			else {
+				terminal = vscode.window.createTerminal();
+				terminal.show();
+				terminal.sendText(`debrewer ${fileName}`);
+			}
+		}
+	));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('pint.debrewAndRun', () => {
+			vscode.commands.executeCommand('pint.debrew');
+			var terminal = vscode.window.activeTerminal;
+			var fileName = vscode.window.activeTextEditor?.document.fileName;
+			// replace current extension with .py
+			var fileName = fileName?.replace(/\.[^/.]+$/, ".py");
+
+			// check if that file exists
+			var fs = require('fs');
+			if (fs.existsSync(fileName)) {
+				if (terminal) {
+					terminal.show();
+					terminal.sendText(`python ${fileName}`);
+				} 
+				else {
+					terminal = vscode.window.createTerminal();
+					terminal.show();
+					terminal.sendText(`python ${fileName}`);
+				}
+			}
+		}
+	));	
+
+
+
+
+
+
 }
 
 export function deactivate() {}
